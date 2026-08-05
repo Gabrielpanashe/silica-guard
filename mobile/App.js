8561216f193b771b284dde7095eff41b8e5654a3
MUKARO JR <takukelvin01@gmail.com>
2026-08-05 02:15:13 +0200

    feat(mobile): add IntakeScreen with job role chips, years selector and validation

--- mobile/App.js
+++ mobile/App.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
 import HomeScreen from './src/screens/HomeScreen';
 // Import other screens here as you build them:
-// import IntakeScreen from './src/screens/IntakeScreen';
+import IntakeScreen from './src/screens/IntakeScreen';
 // import QuestionScreen from './src/screens/QuestionScreen';
 // import ResultScreen from './src/screens/ResultScreen';
 // import ReferralScreen from './src/screens/ReferralScreen';
@@ -16,7 +16,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home"     component={HomeScreen} />
         {/* Add screens below as you build them: */}
-        {/* <Stack.Screen name="Intake"   component={IntakeScreen} /> */}
+        { <Stack.Screen name="Intake"   component={IntakeScreen} /> }
         {/* <Stack.Screen name="Question" component={QuestionScreen} /> */}
         {/* <Stack.Screen name="Result"   component={ResultScreen} /> */}
         {/* <Stack.Screen name="Referral" component={ReferralScreen} /> */}
